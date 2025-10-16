ea17044e05ec44718e84c92f7d0ecc952b86d758
shlxl <101192758+shlxl@users.noreply.github.com>
2025-10-16 10:10:54 +0800

    fix: copy default locale content into dist

--- docs/.vitepress/theme/components/LocaleToggleButton.vue
+++ docs/.vitepress/theme/components/LocaleToggleButton.vue
@@ -2,24 +2,25 @@
 import { computed } from 'vue'
 import { useLocaleToggle } from '../composables/localeMap'
 
-const { currentLocale, destination, goToTarget, hasMapping } = useLocaleToggle()
+const { currentLocale, destination, goToTarget, hasMapping, canNavigate } = useLocaleToggle()
 
 const buttonText = computed(() => (currentLocale.value === 'en' ? '中文' : 'EN'))
 const ariaLabel = computed(() => (currentLocale.value === 'en' ? '切换到中文内容' : 'Switch to English content'))
 
 function handleToggle() {
-  if (!hasMapping.value) return
+  if (!canNavigate.value) return
   goToTarget()
 }
 </script>
 
 <template>
   <button
-    v-if="hasMapping"
+    v-if="canNavigate"
     class="la-locale-toggle"
     type="button"
     :aria-label="ariaLabel"
     :data-target="destination"
+    :data-has-mapping="hasMapping"
     @click="handleToggle"
   >
     <span class="la-locale-toggle__label">{{ buttonText }}</span>
