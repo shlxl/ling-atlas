1ddb7f4298672eae58f9e74087739ec1e7b79cef
shlxl <101192758+shlxl@users.noreply.github.com>
2025-10-17 12:23:40 +0800

    fix: ensure default theme styles load on first render (#141)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,15 @@
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/dist/client/theme-default/without-fonts'
 import Layout from './Layout.vue'
-import VPBadge from 'vitepress/dist/client/theme-default/components/VPBadge.vue'
 import './vars.css'
-import 'vitepress/dist/client/theme-default/styles/base.css'
-import 'vitepress/dist/client/theme-default/styles/utils.css'
-import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 import './custom.css'
 
-export default {
+const theme: Theme = {
+  ...DefaultTheme,
   Layout,
-  enhanceApp({ app }) {
-    app.component('Badge', VPBadge)
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp?.(ctx)
   }
 }
+
+export default theme
