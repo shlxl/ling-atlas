efa4d734640709fb753e8a69a2cd351beea82541
shlxl <101192758+shlxl@users.noreply.github.com>
2025-10-18 16:38:20 +0800

    fix: correct nav bar composable imports

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,9 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/dist/client/theme-default/without-fonts'
 import Layout from './Layout.vue'
+import VPNavBarExtra from './components/VPNavBarExtra.vue'
+import VPNavBarTranslations from './components/VPNavBarTranslations.vue'
+import VPNavScreenTranslations from './components/VPNavScreenTranslations.vue'
 import './vars.css'
 import './custom.css'
 
@@ -9,6 +12,9 @@ const theme: Theme = {
   Layout,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp?.(ctx)
+    ctx.app.component('VPNavBarExtra', VPNavBarExtra)
+    ctx.app.component('VPNavBarTranslations', VPNavBarTranslations)
+    ctx.app.component('VPNavScreenTranslations', VPNavScreenTranslations)
   }
 }
 
