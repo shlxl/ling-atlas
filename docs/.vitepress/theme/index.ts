eee5f527322f059c8cbca2341f8cb14ea9c1735b
shlxl <101192758+shlxl@users.noreply.github.com>
2025-10-18 16:56:34 +0800

    fix: override nav components and adjust lighthouse preset (#186)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,10 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/dist/client/theme-default/without-fonts'
 import Layout from './Layout.vue'
+import VPNavBar from './components/VPNavBar.vue'
 import VPNavBarExtra from './components/VPNavBarExtra.vue'
 import VPNavBarTranslations from './components/VPNavBarTranslations.vue'
+import VPNavScreen from './components/VPNavScreen.vue'
 import VPNavScreenTranslations from './components/VPNavScreenTranslations.vue'
 import './vars.css'
 import './custom.css'
@@ -12,8 +14,10 @@ const theme: Theme = {
   Layout,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp?.(ctx)
+    ctx.app.component('VPNavBar', VPNavBar)
     ctx.app.component('VPNavBarExtra', VPNavBarExtra)
     ctx.app.component('VPNavBarTranslations', VPNavBarTranslations)
+    ctx.app.component('VPNavScreen', VPNavScreen)
     ctx.app.component('VPNavScreenTranslations', VPNavScreenTranslations)
   }
 }
